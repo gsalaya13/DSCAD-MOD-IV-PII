5abbe3b770efca9007dec05cd6d05e0fa24364c3
g.salaya.13 <105703761+gsalaya13@users.noreply.github.com>
2026-08-08 12:26:43 -0600

    Update Titanic.py

--- Titanic.py
+++ Titanic.py
@@ -6,7 +6,7 @@ from sklearn.tree import DecisionTreeClassifier
 
 
 st.write(''' # Predicción de sobrevivientes del Titanic ''')
-st.image("billete-titanic.jpg", caption="El Titanic navegaba desde Southampton, Inglaterra, hasta Nueva York en Estados Unidos.")
+st.image("titanicimage.jpeg", caption="El Titanic navegaba desde Southampton, Inglaterra, hasta Nueva York en Estados Unidos.")
 
 st.header('Datos de evaluación')
 
